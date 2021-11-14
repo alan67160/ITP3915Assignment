6d4cad11afe892e33a9d9500d552d45490faa9fb
alan67160 <minecrafac.tommy@yahoo.com>
2021-11-14 20:28:13 +0800

    update VERSION

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
+import urllib.request, os, zipfile, shutil
 
+working_directory = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+newest_ver = str(urllib.request.urlopen("https://raw.githubusercontent.com/alan67160/ITP3915Assignment/master/VERSION").read()).strip("b\'")
+current_ver = open(working_directory + "/VERSION", "r").read()
+if not (current_ver == newest_ver):
+    print(f"The current application version is {current_ver}. (newest:{newest_ver})")
+    urllib.request.urlretrieve("https://github.com/alan67160/ITP3915Assignment/archive/refs/heads/master.zip", working_directory + "/update.zip")
+    with zipfile.ZipFile(working_directory + "/update.zip", 'r') as update_zip:
+        update_zip.extractall(working_directory + "/tmp")
+    for file in os.listdir(working_directory + "/tmp/ITP3915Assignment-master"):
+        shutil.move(os.path.join(working_directory + "/tmp/ITP3915Assignment-master", file), os.path.join(working_directory, file))
+    shutil.rmtree(working_directory + "/tmp")
