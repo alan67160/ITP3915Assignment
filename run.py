d5ca1c22658c5d56c1029eeb4036d5364dc08727
alan67160 <minecrafac.tommy@yahoo.com>
2021-11-14 21:00:01 +0800

    update

--- run.py
+++ run.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 import urllib.request, os, zipfile, shutil
 
+
+# environment variables
 working_directory = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
-newest_ver = str(urllib.request.urlopen("https://raw.githubusercontent.com/alan67160/ITP3915Assignment/master/VERSION").read()).strip("b\'")
-current_ver = open(working_directory + "/VERSION", "r").read()
-if not (current_ver == newest_ver):
-    print(f"The current application version is {current_ver}. (newest:{newest_ver})")
-    urllib.request.urlretrieve("https://github.com/alan67160/ITP3915Assignment/archive/refs/heads/master.zip", working_directory + "/update.zip")
-    with zipfile.ZipFile(working_directory + "/update.zip", 'r') as update_zip:
-        update_zip.extractall(working_directory + "/tmp")
-    os.remove(working_directory + "/update.zip")
-    for file in os.listdir(working_directory + "/tmp/ITP3915Assignment-master"):
-        try:
-            shutil.move(os.path.join(working_directory + "/tmp/ITP3915Assignment-master", file), os.path.join(working_directory, file))
-        except:
-            pass
-    shutil.rmtree(working_directory + "/tmp")
-    print("update completed")
+update_url = "https://raw.githubusercontent.com/alan67160/ITP3915Assignment/master/VERSION"
+repository_url = "https://github.com/alan67160/ITP3915Assignment"
+
+
+# update
+def update():
+    newest_ver = str(urllib.request.urlopen(update_url).read()).strip("b\'")
+    current_ver = open(working_directory + "/VERSION", "r").read()
+    if not (current_ver == newest_ver):
+        print(f"The current application version is {current_ver}. (newest:{newest_ver})")
+        print("Newer version found! Attempting to update...")
+        urllib.request.urlretrieve(repository_url + "/archive/refs/heads/master.zip", working_directory + "/update.zip")
+        with zipfile.ZipFile(working_directory + "/update.zip", 'r') as update_zip:
+            update_zip.extractall(working_directory + "/tmp")
+        os.remove(working_directory + "/update.zip")
+        for file in os.listdir(working_directory + "/tmp/ITP3915Assignment-master"):
+            try:
+                shutil.move(os.path.join(working_directory + "/tmp/ITP3915Assignment-master", file), os.path.join(working_directory, file))
+            except:
+                pass
+        shutil.rmtree(working_directory + "/tmp")
+        print("update completed!")
+
+def main():
+    from app import main
